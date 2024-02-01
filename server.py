4e3f274b552ecbf983011860fbec31566008b7ae
Vinícius Martins <vinicius.pereira.mts@gmail.com>
2024-02-01 14:36:53 +0000

    Configurando flask

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 from scripts import *
+from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+import os
 
-if __name__ == '__main__':
+app = Flask(__name__)
 
+def make_verifications():
     data = get_data_Notion_API()
     new_query_API = transform_API_data(data)
     
@@ -12,3 +16,19 @@ if __name__ == '__main__':
         saved_query = load_save_json()
 
     verify_modifications(new_query_API, saved_query)
+
+@app.route('/')
+def home():
+    return "Verificando o status do projeto..."
+
+if __name__ == '__main__':
+    scheduler = BackgroundScheduler()
+    scheduler.add_job(func=make_verifications, trigger='interval', seconds=10)
+    scheduler.start()
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+    try:
+        app.run(port=5000, debug=True)
+        
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
